25157efe29cc7bdbcb76ed3b11d77f7c8de64547
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-07-15 00:40:32 +0900

    Update 포인터배열_예제.c

--- Day_01/포인터배열_예제.c
+++ Day_01/포인터배열_예제.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 
-int main() 
+int main()
 {
-	int arr[3][3] = { {10,20,30},{100,200,300},{100,200,300} };
-
-	printf("arr의 주소값 : %p\n", &arr);
+	int num1 = 100;
+	int num2 = 200;
+	int num3 = 300;
+	
+	int* arr[3] = { &num1,&num2 ,&num3 };
 
 	for (int i = 0; i < 3; i++)
 	{
-		printf("arr[%d]의 주소값 : %p\n", i, &arr[i]);
-		for (int j = 0; j < 3; j++)
-		{
-			printf("arr[%d][%d]의 주소값 : %p / 값 : %d\n", i, j, &arr[i][j], *(*(arr + i) + j));
-		}
-	}
+		printf("arr[%d] = 주소값 : %p / 값 : %d\n", i, arr[i], *arr[i]);
 
+	}
 	return 0;
 }
