87d012c5d5b102f9252f3f55b9ab36a4d343e215
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-07-24 00:20:06 +0900

    Create 다양한_생성자.cpp

---
+++ Day_08/code/다양한_생성자.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class SimpleClass
+{
+private:
+	int num1;
+	int num2;
+public:
+	SimpleClass()
+	{
+		num1 = 0;
+		num2 = 0;
+	}
+	SimpleClass(int n)
+	{
+		num1 = n;
+		num2 = 0;
+	}
+	SimpleClass(int n1, int n2)
+	{
+		num1 = n1;
+		num2 = n2;
+	}
+	void Show() const
+	{
+		cout << num1 << ' ' << num2 << endl;
+	}
+};
+
+int main()
+{
+	SimpleClass sc1;
+	sc1.Show();
+
+	SimpleClass sc2(100);
+	sc2.Show();
+
+	SimpleClass sc3(100, 200);
+	sc3.Show();
+
+	return 0;
+}
