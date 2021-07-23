ee93237888e42dc8a1bb81051f7a749bea1e8ba8
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-07-24 00:20:57 +0900

    Create 생성자_매개변수_디폴트(default)_값.cpp

---
+++ Day_08/code/생성자_매개변수_디폴트(default)_값.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class SimpleClass
+{
+private:
+	int num1;
+	int num2;
+public:
+	SimpleClass(int n1 = 0, int n2 = 0)
+	{
+		num1 = n1;
+		num2 = n2;
+	}
+	void Show() const
+	{
+		cout << num1 << ' ' << num2 << endl;
+	}
+};
+
+int main()
+{
+	SimpleClass sc1;
+	sc1.Show();
+
+	SimpleClass sc2(100);
+	sc2.Show();
+
+	SimpleClass sc3(100, 200);
+	sc3.Show();
+
+	return 0;
+}
